23cd3d6f2f24361ab4afb5ad753e7d8b46bd7069
Hanson-HSChang <hanson.hschang@gmail.com>
2024-06-01 18:18:45 -0500

    feat: add continuous actuation base class

---
+++ src/cobr2/actuations/__init__.py
@@ -0,0 +1,1 @@
+from .actuation import *
---
+++ tests/test_actuation.py
@@ -0,0 +1,31 @@
+from cobr2.actuations import ContinuousActuation
+
+
+def test_actuation_shape():
+
+    n_dim = 3
+    n_elements = 10
+    actuation = ContinuousActuation(n_elements=n_elements)
+
+    assert actuation.internal_force.shape == (n_dim, n_elements)
+    assert actuation.internal_couple.shape == (n_dim, n_elements - 1)
+    assert actuation.external_force.shape == (n_dim, n_elements + 1)
+    assert actuation.external_couple.shape == (n_dim, n_elements)
+
+
+def test_actuation_reset():
+
+    n_elements = 10
+    actuation = ContinuousActuation(n_elements=n_elements)
+
+    actuation.internal_force[0, :] = 1
+    actuation.internal_couple[0, :] = 1
+    actuation.external_force[0, :] = 1
+    actuation.external_couple[0, :] = 1
+
+    actuation.reset_actuation()
+
+    assert (actuation.internal_force == 0).all()
+    assert (actuation.internal_couple == 0).all()
+    assert (actuation.external_force == 0).all()
+    assert (actuation.external_couple == 0).all()
