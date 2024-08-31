52cc11c9d3e83ecd56c55d7dbc38863c4fb47a58
tixianw <tixianw@gmail.com>
2024-08-30 22:05:54 -0500

    update collect_br2_data.py

--- examples/collect_br2_data.py
+++ examples/collect_br2_data.py
@@ -2,9 +2,7 @@
 Created on Jun 01, 2024
 @author: Heng-Sheng (Hanson) Chang
 """
-
 import sys
-
 import numpy as np
 from set_br2_environment import BR2Environment
 from tqdm import tqdm
@@ -31,17 +29,15 @@ def main(
     twist_max = 30
     idx = int(idx)
     n_interval = 5
-    bends = np.linspace(0, bend_max, n_interval + 1)
-    twists = np.linspace(0, twist_max, n_interval + 1)
-    bend_twist_pair = np.hstack(
-        [
-            np.vstack([np.ones(len(bends)) * bend_max, twists]),
-            np.vstack([bends, np.ones(len(twists)) * twist_max]),
-        ]
-    )[:, :-1]
+    bends = np.linspace(0, bend_max, n_interval+1)
+    twists = np.linspace(0, twist_max, n_interval+1)
+    bend_twist_pair = np.hstack([
+        np.vstack([np.ones(len(bends))*bend_max, twists]),
+        np.vstack([bends, np.ones(len(twists))*twist_max])
+    ])[:,:-1]
     bend = bend_twist_pair[0, idx]
     CWtwist = bend_twist_pair[1, idx]
-    print("bend:", bend, "twist:", CWtwist)
+    print('bend:', bend, 'twist:', CWtwist)
 
     # Start the simulation
     print("Running simulation ...")
@@ -50,10 +46,7 @@ def main(
         bending = min(bend * time, bend)
         CWtwisting = min(CWtwist * time, CWtwist)
         time = env.step(
-            time=time,
-            pressures=np.array(
-                [bending, CWtwisting, 0.0]
-            ),  # [bending, 0.0, CWtwisting]
+            time=time, pressures=np.array([bending, CWtwisting, 0.0]) # [bending, 0.0, CWtwisting]
         )
         # if (step+1) % 10000 == 0:
         #     print(np.linalg.norm(env.rod.velocity_collection))
@@ -67,7 +60,7 @@ def main(
         os.mkdir(folder_name)
 
     print("saving data...")
-    env.save(folder_name + "/BR2_simulation%02d" % (idx))  # +11
+    env.save(folder_name+"/BR2_simulation%02d" % (idx)) # +11
 
 
 if __name__ == "__main__":
