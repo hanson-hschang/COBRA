af4a13562fe6cc65aab9008a2d85d6d1d7ed047f
Hanson-HSChang <hanson.hschang@gmail.com>
2024-10-13 16:26:10 -0500

    wip: update set_br2_environment.py

--- examples/set_br2_environment.py
+++ examples/set_br2_environment.py
@@ -22,6 +22,8 @@ try:
 
     if Version(bsr.version) < Version("0.1.1"):
         raise ImportError("BSR version should be at least 0.1.1")
+
+    from bsr.geometry.composite.pose import Pose
 except ImportError:
     BSR_AVAILABLE = False
 
@@ -324,7 +326,7 @@ class BR2Environment(BaseEnvironment):
 def main(
     final_time: float = 5.0,
     time_step: float = 1.0e-5,
-    recording_fps: int = 30,
+    recording_fps: int = 60,
 ):
     # Initialize the environment
     env = BR2Environment(
@@ -333,16 +335,64 @@ def main(
         recording_fps=recording_fps,
     )
 
+    if BSR_AVAILABLE:
+        camera_direction_angle = -45 / 180 * np.pi
+        camera_distance = 0.9
+        camera_height = 0.5
+        camera_direction = np.array(
+            [
+                np.cos(camera_direction_angle),
+                np.sin(camera_direction_angle),
+                0.0,
+            ]
+        )
+
+        bsr.camera.location = camera_distance * camera_direction + np.array(
+            [0.0, 0.0, camera_height]
+        )
+        bsr.camera.look_at = np.array([0.0, 0.0, -0.15])
+        bsr.camera.set_film_transparent()
+        bsr.camera.set_resolution(1920, 1080)
+        bsr.camera.set_file_path(
+            file_name="br2",
+            folder_path="video/frames",
+        )
+
+        pose_obj = Pose(
+            positions=np.zeros(3),
+            directors=env.rod.director_collection[:, :, 0].T,
+            unit_length=0.03,
+        )
+        pose_obj.update_material(
+            color=np.array([64, 64, 64, 256], dtype=np.float64) / 256.0
+        )
+
+        # Set the current frame number
+        bsr.frame_manager.frame_current = 0
+
+        # Set the initial keyframe number
+        bsr.frame_manager.set_frame_start()
+
     # Start the simulation
     print("Running simulation ...")
     time = np.float64(0.0)
     for step in tqdm(range(env.total_steps)):
         time = env.step(
-            time=time, pressures=np.array([30 * time, 30 * time, 0.0])
+            time=time,
+            pressures=np.array(
+                [
+                    0.0,
+                    0.0,
+                    30 * time,
+                ]
+            ),
         )
     print("Simulation finished!")
 
     if BSR_AVAILABLE:
+        # Set the frame end
+        bsr.frame_manager.set_frame_end()
+
         # Set the frame rate
         bsr.frame_manager.set_frame_rate(fps=recording_fps)
 
@@ -353,7 +403,10 @@ def main(
         bsr.deselect_all()
 
         # Select the camera object
-        bsr.select_camera()
+        bsr.camera.select()
+
+        # Render the camera frames
+        bsr.camera.render(np.arange(bsr.frame_manager.frame_end))
 
     # Save the simulation
     env.save("BR2_simulation")
@@ -361,3 +414,5 @@ def main(
 
 if __name__ == "__main__":
     main()
+
+# ffmpeg -threads 8 -r 60 -i video/frames/br2_%03d.png -b:v 90M -c:v prores -pix_fmt yuva444p10le video/br2.mov
